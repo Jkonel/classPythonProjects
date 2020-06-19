5f7263ec19569c65fd9816a86ac15631d9c16d37
= <17853940199jhk@sina.com>
2020-06-19 11:20:11 +0800

    day8_1

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 @Author: Jkonel
 @Date: 2020-06-18 09:54:24
 @LastEditors: jkonel
-@LastEditTime: 2020-06-18 17:46:34
+@LastEditTime: 2020-06-19 10:27:10
 '''
 
 import requests
@@ -19,14 +19,15 @@ import my_reptile
 
 my_rep = my_reptile.my_reptile()
 
-rang = 95+1
+rang = 20+1
+
+# python java c/c++ go php   linux 嵌入式软件 嵌入式硬件
 
 for i in range(1,rang):
-    url = 'http://search.jumei.com/?filter=0-11-{0}&search=%E5%8F%A3%E7%BA%A2&from=search_toplist_%E5%8F%A3%E7%BA%A2_word_pos_6&cat='.format(i)
-    name = 'page_1_{0}.html'.format(i)
+    url = 'https://search.51job.com/list/000000,000000,0000,00,9,99,%25E5%25B5%258C%25E5%2585%25A5%25E5%25BC%258F%25E7%25A1%25AC%25E4%25BB%25B6%25E5%25B7%25A5%25E7%25A8%258B%25E5%25B8%2588,2,{0}.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare='.format(i)
+    name = 'page_8_{0}.html'.format(i)
     my_rep.page_download(url, name)
     
-
 
 # 子页面读取
 
@@ -56,3 +57,4 @@ for i in range(1,rang):
 #---------------文件保存----------------#
 #df = pd.DataFrame(data)
 #df.to_csv('./htmls/data.csv',index = 0,na_rep = 'NA')
+
